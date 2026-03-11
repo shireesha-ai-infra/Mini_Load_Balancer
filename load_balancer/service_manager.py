6ca45ac4ee3728853eb3e036daad334bf65161e9
Shireesha Govindu <shireeshagovindu24@gmail.com>
2026-03-11 13:10:39 +0530

    Updated server_manager.py with start, stop and reconcile the servers and also handled the manager crash

--- load_balancer/service_manager.py
+++ load_balancer/service_manager.py
@@ -19,32 +19,48 @@ def load_registry():
 
     return servers
 
-def reconcile_servers():
-    global processes
+def start_server(server_id, port):
+    print(f"Starting {server_id} on {port}")
 
-    desired = load_registry()
+    p = subprocess.Popen(
+        ["python", "server.py", server_id, str(port)]
+    )
 
-    # Starting new servers
-    for server_id, port in desired.items():
+    processes[server_id] = {
+        "process":p,
+        "port": port
+    }
+
+def stop_server(server_id):
+    print(f"Stopping {server_id}")
+
+    processes[server_id]["process"].terminate()
+    del processes[server_id]
+
+
+def reconcile():
+    desired_servers = load_registry()
+
+    running_servers = list(processes.keys())
+
+    # Start missing servers
+    for server_id, port in desired_servers.items():
         if server_id not in processes:
-            print(f"Starting {server_id} on port {port}")
-
-            p = subprocess.Popen(
-                ["python", "server.py", server_id, str(port)]
-            )
-            processes[server_id] = p
+            start_server(server_id, port)
 
     # Stop removed servers
-    for server_id in list(processes.keys()):
-        if server_id not in desired:
-            print(f"Stopping {server_id}")
-            processes[server_id].terminate()
-            del processes[server_id]
+    for server_id in running_servers:
+        if server_id not in desired_servers:
+            stop_server(server_id)
 
 
 if __name__ == "__main__":
     print("Server Manager Started")
 
     while True:
-        reconcile_servers()
+        try:
+            reconcile()
+        except Exception as e:
+            print("Manager Error: ",e)
+            
         time.sleep(3)
